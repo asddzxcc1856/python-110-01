b143a2d069b33c85f70c1d4773bb026962914f9a
嚕梯 <59413200+asddzxcc1856@users.noreply.github.com>
2021-09-30 23:27:07 +0800

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,1 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dic = {
+    "col 1": [1, 2, 3], 
+    "col 2": [10, 20, 30],
+    "col 3": list('xyz'),
+    "col 4": ['a', 'b', 'c'],
+    "col 5": pd.Series(range(3))
+}
+df = pd.DataFrame(dic)
+print(df)
+
+a = np.arange(10)
+
+print(a)
+
 print(1)
